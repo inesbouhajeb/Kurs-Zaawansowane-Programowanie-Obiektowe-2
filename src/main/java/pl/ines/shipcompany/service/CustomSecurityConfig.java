e0ba29c36ad89390ba017b66aae19cc55c377044
ines <i.bouhajeb@gmail.com>
2018-12-06 02:20:25 +0100

    spring secutity development

--- src/main/java/pl/ines/shipcompany/service/CustomSecurityConfig.java
+++ src/main/java/pl/ines/shipcompany/service/CustomSecurityConfig.java
@@ -1,4 +1,28 @@
 package pl.ines.shipcompany.service;
 
-public class CustomSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+public class CustomSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        UserDetails user = User.withUsername("user2").password("{noop}pass2").roles("USER").build();
+        auth.inMemoryAuthentication().withUser(user);
+    }
 }
